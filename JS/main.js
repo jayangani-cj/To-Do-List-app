cdf2b631981ddc75548aa37cd61a3295ffb89c92
Chathura Madhava <87842429+P-A-Chathura-Madhava@users.noreply.github.com>
2025-02-05 00:51:51 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -90,8 +90,8 @@ async function fetchTodos() {
     console.log("Fetching")
     try {
         const response = await fetch(`https://to-do-list-app-cnry.onrender.com/api/todos?page=${currentPage}&limit=${itemsPerPage}`);
-        console.log("Logs : ", response);
         const todos = await response.json();
+        console.log("Logs : ", todos);
 
         if (currentPage === 1) { 
             todoList.innerHTML = '';
